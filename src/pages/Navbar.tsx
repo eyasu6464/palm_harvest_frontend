2dcea7f391d4b2af6c066e33f944d3a9de06c792
blackneb <antenehcs@gmail.com>
2023-11-24 11:20:16 +0300

    nav config

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,10 +1,40 @@
 import AukmarHorizontal from '../images/AumkarHorizontal.png'
+import { Dropdown, Avatar } from 'antd';
+import { LogoutOutlined, KeyOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
 import { useSelector } from "react-redux"
 import { Link } from 'react-router-dom'
-import { Avatar } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
 const Navbar = () => {
     const userType = useSelector((state:any) => state.userInformation.user_type);
+    const items: MenuProps['items'] = [
+        {
+          key: '1',
+          label: (
+            <Link to='/profile' >
+              My Profile
+            </Link>
+          ),
+          icon: <UserOutlined />,
+        },
+        {
+          key: '2',
+          label: (
+            <Link to='/changepassword' >
+              Change Password
+            </Link>
+          ),
+          icon: <KeyOutlined />,
+        },
+        {
+          key: '3',
+          label: (
+            <div>
+              <p>Logout</p>          
+            </div>
+          ),
+          icon: <LogoutOutlined />,
+        },
+      ];
     const Menus = [
         { title: 'Users', path:'/users', auth:"Manager"},
         { title: 'Branches', path: '/branches', auth:"Manager" },
@@ -17,7 +47,7 @@ const Navbar = () => {
         <nav className="bg-white border-white h-20 dark:bg-white shadow-lg">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <div className='flex flex-row justify-between'>
-                    <Link to="/home">
+                    <Link to="/">
                         <div className="flex items-center space-x-3 rtl:space-x-reverse">
                             <img src={AukmarHorizontal} className="h-12" alt="Aumkar Logo" />
                             <span className="self-center text-2xl text-gray-700 font-semibold whitespace-nowrap">Aumkar</span>
@@ -38,7 +68,9 @@ const Navbar = () => {
                     </div>
             </div>
             <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <Avatar size={36} icon={<UserOutlined />} />
+                    <Dropdown menu={{ items }} placement="bottom" arrow>
+                      <Avatar className='mx-8' style={{ backgroundColor: '#fde3cf', color: '#f56a00' }}>U</Avatar>
+                    </Dropdown>
                 <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
