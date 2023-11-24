040abd955a215e8183aed5f9a910f0e37f20f7e8
blackneb <antenehcs@gmail.com>
2023-11-24 09:34:29 +0300

    Designs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,8 +5,8 @@ import AumkarVertival from '../images/AumkarVertical.png'
 const Login = () => {
   return (
     <div>
-        <section className="flex flex-col md:flex-row h-screen rounded-2xl justify-center items-center">
-            <div className="bg-white shadow-md w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3  px-6 lg:px-16 xl:px-12 flex items-center justify-center">
+        <section className="flex flex-col md:flex-row h-screen justify-center items-center">
+            <div className="bg-white shadow-md px-8 w-1/3 flex items-center rounded-2xl justify-center">
                 <div className="w-full h-100">
                 <div className='flex align-center justify-center mb-8'>
                 <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12 text-gray-700">Aumkar</h1>
